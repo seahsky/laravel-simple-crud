a206a97ee451786b39722a41229cf7c2a34d5bbd
seahsky <seahsky97@gmail.com>
2019-03-25 16:19:19 +0800

    added function to seed db data automatically

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/reset', function () {
+    \Artisan::call('migrate:refresh',['--seed' => ' ']);
+    return back();
+})->name('reset');
 
 Auth::routes();
 
